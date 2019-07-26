21ee00611c603e4d26571b476e072219341b8f90
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-07-26 17:47:06 +0100

    name and email in the form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,33 +1,51 @@
 import React, { Component } from 'react';
 import './styles.css';
 
+
 export default class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email:''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
     
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    this.setState({
+      [name]: value
+    });
+    console.log('Change detected. State updated' + name + ' = ' + value);
+  }
+
+  handleSubmit(event) {
+    alert('A form was submitted: ' + this.state.name + ' // ' + this.state.email);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit} >
+          <div className="form-group">
+            <label htmlFor="nameImput">Name</label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} className="form-control" id="nameImput" placeholder="Name" />
+          </div><br />
+          <div className="form-group">
+            <label htmlFor="emailImput">Email</label>
+            <input name="email" type="email" value={this.state.email} onChange={this.handleChange} className="form-control" id="emailImput" placeholder="email@domain.com" />
+          </div><br />
+          <input type="submit" value="Submit" className="btn btn-primary" />
+        </form>
+      </div>
+    )
+  }
       }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
-    
-      render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                Name:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-      }
-}
+
